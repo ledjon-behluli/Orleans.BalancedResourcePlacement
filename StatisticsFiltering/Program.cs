452c8f16452f2034b5cfb3778d7b40d6a2d344d5
Ledjon Behluli <Ledjon@notiphy.io>
2023-12-28 22:57:23 +0100

    .

--- StatisticsFiltering/Program.cs
+++ StatisticsFiltering/Program.cs
@@ -2,43 +2,45 @@
 using System.Reflection;
 using Orleans.BalancedResourcePlacement;
 
-const int iterations = 2500;
-const bool save = true;
+const bool saveResultForPlotting = true;
+const int iterations = 250;
 
 var filter = new StatisticsFilter<float>();
-var table = new Table();
-
-table.AddColumn("Iteration");
-table.AddColumn("Simulated");
-table.AddColumn("Filtered");
-table.AddColumn("Diff (%)");
 
 float cpuIncrement = 0.1f;
 float simulatedCpuUsage = 5.0f;
 
-using (StreamWriter writer = new("output.txt"))
+bool _1stFlag = false;
+bool _2ndFlag = false;
+List<(int, int)> trafficHours =
+      //[(0, 1), (16, 24), (39, 40)];
+      //[(0, 8), (20, 21), (40, 48)];
+      //[(0, 2), (6, 8), (12, 14), (18, 22)];
+      //[(0, 2), (4, 6), (8, 10), (12, 14), (16, 18), (20, 22)];
+      [(0, 1), (3, 4), (6, 7), (9, 10), (12, 13), (15, 16), (18, 19), (21, 22), (24, 25), (27, 28), (30, 31), (33, 34), (36, 37), (39, 40), (42, 43), (45, 46), (48, 49)];
+int maxHour = trafficHours.SelectMany(pair => new[] { pair.Item1, pair.Item2 }).Max();
+
+Action<int> patternInvoker = new(iteration =>
 {
-    foreach (string column in new string[] { "Iteration", "Simulated", "Filtered", "Diff (%)" })
-    {
-        writer.Write(column + "\t");
-    }
+    //UsagePattern.LinearIncreaseLinearDecrease(ref cpuIncrement, ref simulatedCpuUsage);
+    //UsagePattern.LinearIncreaseSharpDecrease(ref cpuIncrement, ref simulatedCpuUsage);
+    //UsagePattern.ExponentialIncreaseLinearDecrease(ref cpuIncrement, ref simulatedCpuUsage);
+    //UsagePattern.LinearIncreaseWithSuddenPeriodicBouncingFluctuations(ref cpuIncrement, ref simulatedCpuUsage, ref _1stFlag);
+    //UsagePattern.LinearIncreaseWithSuddenSingleDownUpFluctuation(ref cpuIncrement, ref simulatedCpuUsage, ref _1stFlag, ref _2ndFlag);
+    UsagePattern.SemiPeriodicHighLowTrafficWithRandomness(ref simulatedCpuUsage, iteration, iterations, maxHour, trafficHours);
+});
 
-    writer.WriteLine();
+if (!saveResultForPlotting)
+{
+    var table = new Table();
 
-    bool _1stFlag = false;
-    bool _2ndFlag = false;
-    List<(int, int)> trafficHours =
-          //[(0, 1), (16, 24), (39, 40)];
-          //[(0, 8), (20, 21), (40, 48)];
-          //[(0, 2), (6, 8), (12, 14), (18, 22)];
-          //[(0, 2), (4, 6), (8, 10), (12, 14), (16, 18), (20, 22)];
-          [(0, 1), (3, 4), (6, 7), (9, 10), (12, 13), (15, 16), (18, 19), (21, 22), (24, 25), (27, 28), (30, 31), (33, 34), (36, 37), (39, 40), (42, 43), (45, 46), (48, 49)];
-    int maxHour = trafficHours.SelectMany(pair => new[] { pair.Item1, pair.Item2 }).Max();
-    var isTrafficTime = UsagePattern.IsHighTraffic(trafficHours);
+    table.AddColumn("Iteration");
+    table.AddColumn("Simulated");
+    table.AddColumn("Filtered");
 
     for (int i = 0; i < iterations; i++)
     {
-        Console.WriteLine("Iteration: " + i);
+        Console.WriteLine($"Iteration: {i}");
 
         float filteredCpuUsage = filter.Filter(simulatedCpuUsage);
         float diff = 100.0f * Math.Abs((filteredCpuUsage - simulatedCpuUsage) / simulatedCpuUsage);
@@ -46,82 +48,106 @@ using (StreamWriter writer = new("output.txt"))
         table.AddRow(
             (i + 1).ToString(),
             Formatter.ForDisplay(simulatedCpuUsage),
-            Formatter.ForDisplay(filteredCpuUsage),
-            Formatter.ForDisplay(diff) + "%");
+            Formatter.ForDisplay(filteredCpuUsage));
 
-        WriteRow(writer,
-            (i + 1).ToString(),
-            Formatter.ForDisplay(simulatedCpuUsage),
-            Formatter.ForDisplay(filteredCpuUsage),
-            Formatter.ForDisplay(diff) + "%");
-
-        //UsagePattern.LinearIncreaseLinearDecrease(ref cpuIncrement, ref simulatedCpuUsage);
-        //UsagePattern.LinearIncreaseSharpDecrease(ref cpuIncrement, ref simulatedCpuUsage);
-        //UsagePattern.ExponentialIncreaseLinearDecrease(ref cpuIncrement, ref simulatedCpuUsage);
-        //UsagePattern.LinearIncreaseWithSuddenPeriodicBouncingFluctuations(ref cpuIncrement, ref simulatedCpuUsage, ref _1stFlag);
-        //UsagePattern.LinearIncreaseWithSuddenSingleDownUpFluctuation(ref cpuIncrement, ref simulatedCpuUsage, ref _1stFlag, ref _2ndFlag);
-        UsagePattern.SemiPeriodicHighLowTrafficWithRandomness(ref simulatedCpuUsage, i, iterations, maxHour, isTrafficTime);
+        patternInvoker(i);
     }
-}
-
-if (save)
-{
-    SaveSamplingForPlotting(iterations);
-}
-else
-{
-    Console.WriteLine("Enter any key to see results...");
-    Console.ReadKey();
 
     AnsiConsole.Write(table);
     Console.ReadKey();
 }
-
-static void SaveSamplingForPlotting(int rowCount)
+else
 {
-    string? filePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-
-    string[] iteration = new string[rowCount];
-    string[] cpuUsageSimulated = new string[rowCount];
-    string[] cpuUsageFiltered = new string[rowCount];
-    string[] differencePercentage = new string[rowCount];
-
-    using (StreamReader reader = new(filePath + "\\output.txt"))
+    using (StreamWriter writer = new("output.txt"))
     {
-        reader.ReadLine();
-
-        for (int i = 0; i < rowCount; i++)
+        foreach (string column in new string[] { "Iteration", "Simulated", "Filtered", "Diff (%)" })
         {
-            string? line = reader.ReadLine();
-            if (line is null)
-            {
-                break;
-            }
+            writer.Write(column + "\t");
+        }
 
-            string[] columns = line.Split('\t');
-            iteration[i] = columns[0];
-            cpuUsageSimulated[i] = columns[1];
-            cpuUsageFiltered[i] = columns[2];
-            differencePercentage[i] = columns[3].Replace("%", "");
+        writer.WriteLine();
+
+        for (int i = 0; i < iterations; i++)
+        {
+            Console.WriteLine($"Iteration: {i}");
+
+            float filteredCpuUsage = filter.Filter(simulatedCpuUsage);
+            float diff = 100.0f * Math.Abs((filteredCpuUsage - simulatedCpuUsage) / simulatedCpuUsage);
+
+            WriteRow(writer,
+                (i + 1).ToString(),
+                Formatter.ForDisplay(simulatedCpuUsage),
+                Formatter.ForDisplay(filteredCpuUsage));
+
+            patternInvoker(i);
         }
     }
 
-    using (StreamWriter writer = new(filePath + "\\samplings.txt"))
-    {
-        writer.WriteLine("iterations = [" + string.Join(", ", iteration) + "]");
-        writer.WriteLine("simulated = [" + string.Join(", ", cpuUsageSimulated) + "]");
-        writer.WriteLine("filtered = [" + string.Join(", ", cpuUsageFiltered) + "]");
-        writer.WriteLine("diff_perc = [" + string.Join(", ", differencePercentage) + "]");
-    }
-}
+    SaveSamplingForPlotting(iterations);
 
-static void WriteRow(StreamWriter writer, params string[] values)
-{
-    foreach (string value in values)
+    static void WriteRow(StreamWriter writer, params string[] values)
     {
-        writer.Write(value + "\t");
+        foreach (string value in values)
+        {
+            writer.Write(value + "\t");
+        }
+        writer.WriteLine();
     }
-    writer.WriteLine();
+
+    static void SaveSamplingForPlotting(int rowCount)
+    {
+        string? filePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+        string[] iteration = new string[rowCount];
+        string[] cpuUsageSimulated = new string[rowCount];
+        string[] cpuUsageFiltered = new string[rowCount];
+
+        using (StreamReader reader = new(filePath + "\\output.txt"))
+        {
+            reader.ReadLine();
+
+            for (int i = 0; i < rowCount; i++)
+            {
+                string? line = reader.ReadLine();
+                if (line is null)
+                {
+                    break;
+                }
+
+                string[] columns = line.Split('\t');
+
+                iteration[i] = columns[0];
+                cpuUsageSimulated[i] = columns[1];
+                cpuUsageFiltered[i] = columns[2];
+            }
+        }
+
+        using (StreamWriter writer = new(filePath + "\\plot.txt"))
+        {
+            writer.WriteLine(
+               $"""
+            import matplotlib.pyplot as plt
+
+            iterations = [{string.Join(", ", iteration)}]
+            simulated = [{string.Join(", ", cpuUsageSimulated)}]
+            filtered = [{string.Join(", ", cpuUsageFiltered)}]
+
+            plt.figure(figsize=(100, 60))
+
+            plt.plot(iterations, simulated, marker='o', label='Simulated')
+            plt.plot(iterations, filtered, marker='o',  label='Filtered')
+
+            plt.title('Cpu Usage Comparison')
+            plt.xlabel('Iteration')
+            plt.ylabel('Values')
+            plt.legend()
+            plt.grid(True)
+            plt.show()
+            """);
+        }
+
+        File.Delete(filePath + "\\output.txt");
+    }
 }
 
 class Formatter
@@ -255,12 +281,12 @@ class UsagePattern
         int currentIteration,
         int totalIterations,
         int maxHourSpan,
-        Func<int, bool> isHighTraffic)
+        List<(int, int)> trafficHours)
     {
         float percentOfDay = (float)currentIteration / totalIterations;
         int hour = (int)(maxHourSpan * percentOfDay) % maxHourSpan;
 
-        if (isHighTraffic(hour))
+        if (IsHighTraffic(hour, trafficHours))
         {
             cpuUsage = 80f;
             cpuUsage = SuperImposeNoisySin(
@@ -293,10 +319,8 @@ class UsagePattern
                 signalAmplitude * Math.Sin(2 * Math.PI * frequency) +
                 noiseAmplitude * Random.Shared.NextSingle());
         }
-    }
 
-    public static Func<int, bool> IsHighTraffic(List<(int, int)> trafficHours)
-        => (currentHour) =>
+        static bool IsHighTraffic(int currentHour, List<(int, int)> trafficHours)
         {
             foreach (var (startHour, endHour) in trafficHours)
             {
@@ -306,5 +330,6 @@ class UsagePattern
                 }
             }
             return false;
-        };
+        }
+    }
 }
