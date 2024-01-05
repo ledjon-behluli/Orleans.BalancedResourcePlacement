c6affd19077972ffe25c433a614119b9516630f9
Ledjon Behluli <46324828+ledjon-behluli@users.noreply.github.com>
2024-01-05 22:46:03 +0100

    Update BalancedResourcePlacementStrategy.cs

--- Orleans.BalancedResourcePlacement/BalancedResourcePlacementStrategy.cs
+++ Orleans.BalancedResourcePlacement/BalancedResourcePlacementStrategy.cs
@@ -11,8 +11,7 @@ namespace Orleans.BalancedResourcePlacement;
 /// You can adjust the weights based on your specific requirements and priorities for load balancing.</para>
 /// <para>In addition to normalization, an online adaptive filter provides a smoothing effect 
 /// (filters out high frequency components) and avoids rapid signal drops by transforming it into a polynomial alike decay process.
-/// Special emphasis is placed upon the startup of a newly joined silo in relation to the decay process. A polynomial decay closer to being
-/// linear is used at the earlier cycles which subsequently transforms into a more exponential alike decay. This contributes to avoiding resource saturation on the silos and especially newly joined silos.</para>
+/// Special emphasis is placed upon the startup of a newly joined silo in relation to the decay process. This contributes to avoiding resource saturation on the silos and especially newly joined silos.</para>
 /// </summary>
 /// <remarks>
 /// Details of the properties used to make the placement decisions and their default values are given below:
